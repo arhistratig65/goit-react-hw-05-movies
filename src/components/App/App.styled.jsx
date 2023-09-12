546d0cc13dacd8db33d361c84a3b02c864844652
arhistratig65 <118848591+arhistratig65@users.noreply.github.com>
2023-09-13 00:28:02 +0300

    del

--- src/components/App/App.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div`
-  position: relative;
-  margin: 0 auto;
-  max-width: 800px;
-  width: 100%;
-  transition: 1s;
-`;
--- src/util/options.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = '3a61376fa2d4fd76588642066d14799c';
-
-export const BASE_POSTER_URL = 'https://image.tmdb.org/t/p/w500/';
-export const PLACEHOLDER = 'https://via.placeholder.com/182x273';
